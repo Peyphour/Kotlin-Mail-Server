f54976ecd0c23f91acacb9d0c071edca4105018e
Bertrand Nancy <bertrand@slimpay.com>
2017-08-11 11:53:47 +0200

    Filter mail by address in webmail

--- src/main/kotlin/fr/bnancy/mail/web/mail/WebmailController.kt
+++ src/main/kotlin/fr/bnancy/mail/web/mail/WebmailController.kt
@@ -2,6 +2,8 @@ package fr.bnancy.mail.web.mail
 
 import fr.bnancy.mail.repository.MailRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,8 +16,10 @@ class WebmailController {
     lateinit var mailRepository: MailRepository
 
     @RequestMapping
-    fun getMails(model: Model): String {
-        model.addAttribute("mails", mailRepository.findAll().toList())
+    fun getMails(model: Model, auth: Authentication): String {
+        val email = (auth.principal as UserDetails).username
+        val mails = mailRepository.findAll().filter { it.recipients.contains(email) }
+        model.addAttribute("mails", mails)
         return "mail/index"
     }
 }
