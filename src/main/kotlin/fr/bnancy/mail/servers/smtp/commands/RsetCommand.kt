5eb153953ddd9197308c50a20176e7b545816472
Bertrand Nancy <bnancy397@gmail.com>
2017-12-05 22:02:26 +0100

    Fix SMTP state after RSET

--- src/main/kotlin/fr/bnancy/mail/servers/smtp/commands/RsetCommand.kt
+++ src/main/kotlin/fr/bnancy/mail/servers/smtp/commands/RsetCommand.kt
@@ -3,6 +3,7 @@ package fr.bnancy.mail.servers.smtp.commands
 import fr.bnancy.mail.servers.smtp.commands.annotations.SmtpCommand
 import fr.bnancy.mail.servers.smtp.data.SmtpSession
 import fr.bnancy.mail.servers.smtp.data.SmtpResponseCode
+import fr.bnancy.mail.servers.smtp.data.SmtpSessionState
 import fr.bnancy.mail.servers.smtp.listeners.SmtpSessionListener
 
 @SmtpCommand("RSET")
@@ -15,6 +16,8 @@ class RsetCommand: SmtpAbstractCommand {
         smtpSession.from = ""
         smtpSession.content = ""
         smtpSession.receivingData = false
+
+        smtpSession.stateSmtp.add(SmtpSessionState.HELO);
 
         return SmtpResponseCode.OK("OK")
     }
